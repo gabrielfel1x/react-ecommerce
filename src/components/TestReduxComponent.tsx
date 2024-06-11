978d2ddee5cfb144ffb783dc0ae163bb3c77269e
gabrielfel1x <gabrielfelxx@gmail.com>
2024-06-11 07:29:49 -0300

    redux test component

--- src/components/TestReduxComponent.tsx
+++ src/components/TestReduxComponent.tsx
@@ -23,19 +23,22 @@ const TestReduxComponent = () => {
   console.log("Current cart state:", cart);
 
   return (
-    <div className="flex items-center justify-center gap-4 font-bold text-2xl uppercase text-white mt-12 mb-8">
-      <button
-        className="bg-primary rounded-xl p-4 hover:bg-white hover:text-primary transition-all duration-200"
-        onClick={testAddProduct}
-      >
-        Add Product
-      </button>
-      <button
-        className="bg-primary rounded-xl p-4 hover:bg-white hover:text-primary transition-all duration-200"
-        onClick={testRemoveProduct}
-      >
-        Remove Product
-      </button>
+    <div className="flex items-center flex-col justify-center gap-4 font-bold text-2xl uppercase text-white mt-12 mb-8">
+      <h1 className="text-primary">Redux Test</h1>
+      <div className="flex flex-row gap-4">
+        <button
+          className="bg-primary rounded-xl p-4 hover:bg-white hover:text-primary transition-all duration-200"
+          onClick={testAddProduct}
+        >
+          Add Product
+        </button>
+        <button
+          className="bg-primary rounded-xl p-4 hover:bg-white hover:text-primary transition-all duration-200"
+          onClick={testRemoveProduct}
+        >
+          Remove Product
+        </button>
+      </div>
     </div>
   );
 };
